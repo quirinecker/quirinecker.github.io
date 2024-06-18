f7a5f0fa96c2dda7e3bdaa003a306d45a3b7860a
quirinecker <quirin.ecker@bajupa.com>
2024-06-18 22:16:13 +0200

    initial commit

---
+++ src/components/CounterVue.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const count = ref(0)
+
+</script>
+
+<template>
+    <div @click="count++">
+        {{ count }}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
